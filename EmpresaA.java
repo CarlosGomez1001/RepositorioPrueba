3389c3bbf3d5a13bd0965d6d7bd0f4c734b9668e
Carlos Gomez <za19012228@zapopan.tecmm.edu.mx>
2020-11-29 21:40:48 -0600

    Agregando ciclo

--- EmpresaA.java
+++ EmpresaA.java
@@ -1,21 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication15;
 
-/**
- *
- * @author Carlos
- */
+
 public class EmpresaA {
 
-    /**
-     * @param args the command line arguments
-     */
+   
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       Imprime imp = new Imprime();
+        imp.imprime(15);
+        
+        for (int i=0; i<10; i++){
+            imp.imprime(i);
+        }
+        
     }
     
 }
